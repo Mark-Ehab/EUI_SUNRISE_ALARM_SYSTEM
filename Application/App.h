5cb63a2227a2fb4a6e662c18468131d5717e73a7
mohameddmahmoudd <139530368+mohameddmahmoudd@users.noreply.github.com>
2023-09-04 13:49:34 +0300

    Update App.h

--- Application/App.h
+++ Application/App.h
@@ -6,26 +6,24 @@
  *    | |  | |  __/ || (_| |\  /\  / (_| | | |  __/
  *    |_|  |_|\___|\__\__,_| \/  \/ \__,_|_|  \___|
  *==================================================================
-SWC: App.h
-Date: 8/28/2023
-Authors: MetaWare LLC
-         Mahmoud Sayed Mahmoud Helmy (1285)
-         Mohamed Mahmoud Masoud (200)
-         Mark Ehab Tawfik (201)
-         Hazzem Mohamed Ezzeldin (1297)
-         Yousef Khaled Ahmed (558)
-================================================================
+ SWC: App.h
+ Date: 8/28/2023
+ Authors: MetaWare LLC
+ Mahmoud Sayed Mahmoud Helmy (1285)
+ Mohamed Mahmoud Masoud (200)
+ Mark Ehab Tawfik (201)
+ Hazzem Mohamed Ezzeldin (1297)
+ Yousef Khaled Ahmed (558)
+ ================================================================
  Description: This header file contains the declarations and
  function prototypes for the main application logic and functions
  responsible for controlling alarms and user interactions.
-================================================================
-  FILE NAME: App.h
-================================================================*/
-
+ ================================================================
+ FILE NAME: App.h
+ ================================================================*/
 
 #ifndef APP_H_
 #define APP_H_
-
 
 #include <util/delay.h>
 #include "App_Cfg.h"
@@ -38,25 +36,16 @@ Authors: MetaWare LLC
 #define MaximumAlarms 4
 
 /* Position of tens digit for minutes */
-#define MINUTES_TENS 4
+#define TENS_OF_SEC_OR_MIN 5
 
 /* Position of ones digit for minutes */
-#define MINUTES_ONES 3
+#define ONES_OF_SEC_OR_MIN  9
 
-/* Position of tens digit for seconds */
-#define SECONDS_TENS 2
-
-/* Position of ones digit for seconds */
-#define SECONDS_ONES 1
-
-/* Preloaded value for Timer0 */
+/* Pre-loaded value for Timer0 */
 #define TIMER0_PRELOADED_VALUE 0
 
 /* Compare match value for Timer1 */
 #define TIMER1_COMPARE_MATCH_VALUE 15625
-
-/* Maximum value for tens digit of minutes */
-#define MAX_MINUTES_TENS 6
 
 /* Maximum duty cycle for LED dimming */
 #define MAX_DUTY_CYCLE 99
@@ -82,16 +71,33 @@ Authors: MetaWare LLC
 /* Return value indicating continuation */
 #define CONTINUE 0
 
+/* Return value indicating cancelled operation*/
 #define CANCELLED 0
+
+/* Return value indicating exit operation*/
+#define EXIT       2
+
+/* Return value indicating confirmed operation*/
 #define CONFIRMED 1
 
+/* Return value indicating error in operation*/
+#define ERR_STATE   8
+
+/*The minimum value to read from LDR*/
+#define LDR_MIN     0
+
+/* Return value indicating return to main menu operation*/
+#define RETURN_TO_MAIN_MENU 0
+
 /* Enumeration of alarm IDs */
-typedef enum {
+typedef enum
+{
     ALARM_0, ALARM_1, ALARM_2, ALARM_3
 } alarms_id;
 
 /* Structure for alarm configuration */
-typedef struct {
+typedef struct
+{
     uint8 flag;
     int alarmDigits[4];
     int secCnt;
@@ -99,7 +105,8 @@ typedef struct {
 
 /*==================================================================*/
 /**
- * @brief Initializes the application by initializing various drivers and components.
+ * @brief Initializes the application by initializing various drivers
+ *  and components.
  *
  * This function initializes the LCD, keypad, LDR, buzzer, LED, and timers.
  */
@@ -107,21 +114,12 @@ void App_Init(void);
 
 /*==================================================================*/
 /**
- * @brief Displays the main menu options on the LCD.
+ * @brief Manages the main application flow.
  *
- * This function displays the main menu options on the LCD screen.
+ * This function displays the main menu, handles user choices, and executes
+ * corresponding actions.
  */
-void App_mainMenu(void);
-
-/*==================================================================*/
-/**
- * @brief Searches for the first available alarm slot to set the time.
- *
- * @param[out] availableAlarmBuffer A pointer to store the available alarm slot.
- *
- * This function searches for the first available alarm slot and stores its ID in the provided buffer.
- */
-void App_firstAvailableAlarm(uint8* availableAlarmBuffer);
+void MainApp(void);
 
 /*==================================================================*/
 /**
@@ -129,7 +127,7 @@ void App_firstAvailableAlarm(uint8* availableAlarmBuffer);
  *
  * This function guides the user to set the desired time for a new alarm.
  */
-STATUS App_setAlarm(void);
+uint8 App_setAlarm(void);
 
 /*==================================================================*/
 /**
@@ -149,25 +147,62 @@ STATUS App_cancelAlarm(void);
 
 /*==================================================================*/
 /**
- * @brief Manages the main application flow.
- *
- * This function displays the main menu, handles user choices, and executes corresponding actions.
- */
-void App(void);
-
-/*==================================================================*/
-/**
  * @brief Displays a welcome screen with team and project information.
  *
- * This function displays a welcome screen with team names, class information, and a welcome message.
+ * This function displays a welcome screen with team names, class information,
+ * and a welcome message.
  */
 void App_welcomeScreen(void);
 
 /*==================================================================*/
+/**
+ * @brief Contains the logic to be executed in Timer0 overflow routine .
+ *
+ * This function controls the duty cycle of a software PWM that changes every Timer0
+ * overflow interrupt.
+ */
 void LEDdimmingOverFlow(void);
+
+/*==================================================================*/
+/**
+ * @brief Contains the logic to be executed in Timer0 compare match routine .
+ *
+ * This function toggles multiple LEDs every Timer0 compare match interrupt.
+ */
 void LEDdimmingCompareMatch(void);
+
+/*==================================================================*/
+/**
+ * @brief Contains the routine for
+ Timer1 compare match callback function for channel A.
+ *
+ * This function checks for alarm seconds count and sets multiple flags
+ * associated with firing the alarms when the seconds count reaches zero.
+ */
 void Interrupt_function(void);
+
+/*==================================================================*/
+/**
+ * @brief Contains all of the logic to be executed in the event of a fired alarm
+ * and its execution depends on the flags set by the Interrupt_function.
+ *
+ * This function turns on the buzzer, dims multiple LEDS and interacts with the
+ * user through LDR and a puzzle. It turns off the alarm and associated components
+ * in the case of correct input from the user.
+ */
 STATUS alarmAction(void);
+
+/*==================================================================*/
+/**
+ * @brief Contains the logic of a puzzle required to be solved
+ * in the event of a fired alarm.
+ *
+ * This function contains the puzzle to be solved when the associated alarm
+ * fires in the form of a mathematical equation to make sure the
+ * person is fully awake.
+ */
 STATUS WakeUpCalculator(void);
+
+/*==================================================================*/
 
 #endif /* APP_H_ */
